5dd1f45dc4cdde9d50a2592b5a9db58e58d6e407
Mohit <mohitsharma8864@gmail.com>
2023-05-06 00:10:29 +0530

    home page with hover

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -26,21 +26,21 @@ export default function List() {
         <span class="listTitle">Continue to watch</span>
         <div class="wrapper">
             <ArrowBackIosOutlined
-             className='sliderArrow left' 
-             onClick={()=>handleClick("left")} 
-             style={{display: !isMoved && "none"}}
-             />
+              className='sliderArrow left' 
+              onClick={()=>handleClick("left")} 
+              style={{display: !isMoved && "none"}}
+            />
             <div class="container" ref={listRef}>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
+                <ListItem index={0}/>
+                <ListItem index={1}/>
+                <ListItem index={2}/>
+                <ListItem index={3}/>
+                <ListItem index={4}/>
+                <ListItem index={5}/>
+                <ListItem index={6}/>
+                <ListItem index={7}/>
+                <ListItem index={8}/>
+                <ListItem index={9}/>
             </div>
             <ArrowForwardIosOutlined className='sliderArrow right' onClick={()=>handleClick("right")} />
         </div>
