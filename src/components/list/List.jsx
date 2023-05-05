26288f3ccf72fe1473133a468ab71d3063286e70
Mohit <mohitsharma8864@gmail.com>
2023-05-05 23:39:25 +0530

    slider logic

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,17 +1,21 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icons';
 import ListItem from "../listitem/ListItem";
 import './list.scss';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 export default function List() {
+    const [slideNumber, setSlideNumber] = useState(0);
+
     const listRef = useRef();
 
     const handleClick = (direction) => {
         let distance = listRef.current.getBoundingClientRect().x - 50
-        if(direction === "left") {
+        if(direction === "left" && slideNumber > 0) {
+            setSlideNumber(slideNumber - 1)
             listRef.current.style.transform = `translateX(${230 + distance}px)`
         }
-        if(direction === "right") {
+        if(direction === "right" && slideNumber < 5) {
+            setSlideNumber(slideNumber + 1)
             listRef.current.style.transform = `translateX(${-230 + distance}px)`
         }
     }
@@ -21,6 +25,7 @@ export default function List() {
         <div class="wrapper">
             <ArrowBackIosOutlined className='sliderArrow left' onClick={()=>handleClick("left")} />
             <div class="container" ref={listRef}>
+                <ListItem/>
                 <ListItem/>
                 <ListItem/>
                 <ListItem/>
