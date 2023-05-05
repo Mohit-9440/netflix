d933a64958317318de41ffe3e3d81943a8dad0ac
Mohit <mohitsharma8864@gmail.com>
2023-05-05 22:39:52 +0530

    slider logic using react hook

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,14 +1,26 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icons';
 import ListItem from "../listitem/ListItem";
 import './list.scss';
+import { useRef } from 'react';
 
 export default function List() {
+    const listRef = useRef();
+
+    const handleClick = (direction) => {
+        let distance = listRef.current.getBoundingClientRect().x - 50
+        if(direction === "left") {
+            listRef.current.style.transform = `translateX(${230 + distance}px)`
+        }
+        if(direction === "right") {
+            listRef.current.style.transform = `translateX(${-230 + distance}px)`
+        }
+    }
   return (
     <div className='list'>
         <span class="listTitle">Continue to watch</span>
         <div class="wrapper">
-            <ArrowBackIosOutlined className='sliderArrow left'/>
-            <div class="container">
+            <ArrowBackIosOutlined className='sliderArrow left' onClick={()=>handleClick("left")} />
+            <div class="container" ref={listRef}>
                 <ListItem/>
                 <ListItem/>
                 <ListItem/>
@@ -19,7 +31,7 @@ export default function List() {
                 <ListItem/>
                 <ListItem/>
             </div>
-            <ArrowForwardIosOutlined className='sliderArrow right'/>
+            <ArrowForwardIosOutlined className='sliderArrow right' onClick={()=>handleClick("right")} />
         </div>
     </div>
   )
